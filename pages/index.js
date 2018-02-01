4385b7289210822898e57ab287b806bd104fe31a
Lucas <lucas_s.s@hotmail.com>
2018-02-01 18:32:19 -0300

    Fetching dados de um api random e listando os dados o retorno da busca

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,32 @@
-import React, { Component } from 'react'
+import React from 'react'
 import MainLayout from '../src/components/MainLayout'
+import Link from 'next/link'
+import fetch from 'isomorphic-unfetch'
 
-export default class App extends Component {
-  render() {
-    return(
-      <MainLayout>
-        <h1>I'm Index</h1>
-      </MainLayout>
-    )
+const Index = (props) => (
+  <MainLayout>
+    <h1>Batman TV Shows</h1>
+    <ul>
+      {props.shows.map(({show}) => (
+        <li key={show.id}>
+          <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
+            <a>{show.name}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </MainLayout>
+)
+
+Index.getInitialProps = async function() {
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman')
+  const data = await res.json()
+
+  console.log(`Show data fetched. Count: ${data.length}`)
+
+  return {
+    shows: data
   }
 }
+
+export default Index
---
+++ pages/post.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import MainLayout from '../src/components/MainLayout'
+import fetch from 'isomorphic-unfetch'
+
+const Post = (props) => (
+  <MainLayout>
+    <h1>{props.show.name}</h1>
+    <p>{props.show.summary.replace(/<[/]?p>/g, '')}</p>
+    <img src={props.show.image.medium} />
+  </MainLayout>
+)
+
+Post.getInitialProps = async function (context) {
+  const { id } = context.query
+  const res = await fetch(`https://api.tvmaze.com/shows/${id}`)
+  const show = await res.json()
+
+  console.log(`Fetched show: ${show.name}`)
+
+  return { show }
+}
+
+export default Post
